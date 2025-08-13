91f514186729412a8b572a4f1fd329a931a4e4e0
Yishen <yishen.chen@rea-group.com>
2025-08-13 16:05:57 +0800

    init python project and config git

---
+++ src/langgraph101/agent/agent.py
@@ -0,0 +1,23 @@
+from langgraph.prebuilt import create_react_agent
+from langchain.chat_models import init_chat_model
+
+def get_weather(city: str) -> str:  
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+
+model = init_chat_model(
+    "anthropic:claude-3-7-sonnet-latest",
+    temperature=0
+)
+
+agent = create_react_agent(
+    model=model,
+    tools=[get_weather],
+    prompt="You are a helpful assistant"  
+)
+
+# Run the agent
+agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
